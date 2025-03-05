2d9c6e14e2ce936b610f67d229cd919479811c36
SamloStudios <120804576+SamloStudios@users.noreply.github.com>
2025-03-05 02:04:05 -0600

    Update construirListaAct2.py #Act 3 post revision

--- construirListaAct2.py
+++ construirListaAct2.py
@@ -2,26 +2,48 @@ def construir_lista_invertida(documentos):
     indice_invertido = {}
 
     for doc_id, texto in enumerate(documentos, start=1): # Los documentos tienen IDs 1, 2, 3 ...
-        palabras = texto. lower() .split() # Convertimos a minusculas y separamos en palabras
+        palabras = texto.lower().split() # Convertimos a minusculas y separamos en palabras
 
         for palabra in palabras:
             if palabra not in indice_invertido:
-                indice_invertido[palabra] = [] # Si no existe, creamos la lista
+                indice_invertido[palabra] = {} # Si no existe, creamos el diccionario
 
             if doc_id not in indice_invertido[palabra]:
-                indice_invertido[palabra].append(doc_id) # Evitamos duplicados
+                indice_invertido[palabra][doc_id] = 0 # Inicializamos el contador
+
+            indice_invertido[palabra][doc_id] += 1 # Incrementamos el contador
  
     return indice_invertido
 
 def buscarPalabra(index, word):
+    print(index)
     if word in index:
-        print(f"{word}: {index[word]}")
+        print(f"Encontrado ({word}) en: ")
+        for doc, count in index[word].items():
+            print(f"  Documento{doc} ")
     else:
         print(f"{word} no encontrado")
 
+# Extender la implementación para soportar búsquedas de varias palabras.
+# Agregar la capacidad de contar cuántas veces aparece cada palabra en cada documento.
+def buscarPalabras(index, words):
+    if isinstance(words, str):
+        words = words.split()
+    
+    result = ""
+    for word in words:
+        if word in index:
+            result += f"\nPalabra ({word}): conteo de aparicion\n"
+            for doc, count in index[word].items():
+                result += f"  En doc{doc}: {count} veces\n"
+        else:
+            result += f"\n{word} no encontrado\n"
+    print(result)
+        
+
 # Documentos de ejemplo
 documentos = [
-"el gato está durmiendo",
+"el gato perro gato está durmiendo",
 "el perro está jugando",
 "el gato y el perro son amigos"
 ]
@@ -32,4 +54,5 @@ indice = construir_lista_invertida(documentos)
 for palabra, docs in indice.items():
     print(f"{palabra}: {docs}")
     
-buscarPalabra(indice, "gato") #IMPLEMENTADO
+buscarPalabra(indice, "gato") #IMPLEMENTADO ACT2
+buscarPalabras(indice, "perro gato") #IMPLEMENTADO ACT3
